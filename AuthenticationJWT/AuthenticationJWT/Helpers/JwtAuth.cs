0dd38460238f17dba270c3a8237a403c078d641d
Eschechola <lucas.eschechola@outlook.com>
2020-07-13 12:25:38 -0300

    Add project

---
+++ AuthenticationJWT/AuthenticationJWT/Helpers/JwtAuth.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Security.Claims;
+using AuthenticationJWT.Models;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthenticationJWT.Helpers
+{
+    public static class JwtAuth
+    {
+        public static string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            
+            //chave secreta, geralmente se coloca em arquivo de configuração
+            var key = Encoding.ASCII.GetBytes("ZWRpw6fDo28gZW0gY29tcHV0YWRvcmE=");
+            
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Name.ToString()),
+                    new Claim(ClaimTypes.Role, RoleFactory(user.Type))
+                }),
+                Expires = DateTime.UtcNow.AddHours(10),
+
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            
+            return tokenHandler.WriteToken(token);
+        }
+
+        private static string RoleFactory(int roleNumber)
+        {
+            switch (roleNumber)
+            {
+                case 1:
+                    return "Director";
+
+                case 2:
+                    return "Manager";
+
+                case 3:
+                    return "Colaborator";
+
+                default:
+                    throw new Exception();
+            }
+        }
+    }
+}
---
+++ AuthenticationJWT/AuthenticationJWT/Repositories/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using AuthenticationJWT.Models;
+using System.Collections.Generic;
+
+namespace AuthenticationJWT.Repositories
+{
+    public class UserRepository
+    {
+
+        /*
+         * 1 - Diretor
+         * 2 - Gerente
+         * 3 - Colaborador
+         */
+
+        public static IList<User> Users = new List<User>
+        {
+
+            new User
+            {
+                Type = 1,
+                Email = "lucas@eu.com",
+                Name = "Lucas",
+                Password = "123"
+            },
+
+            new User
+            {
+                Type = 2,
+                Email = "mariana@eu.com",
+                Name = "Mariana",
+                Password = "123"
+            },
+
+            new User
+            {
+                Type = 2,
+                Email = "gabriel@eu.com",
+                Name = "Gabriel",
+                Password = "123"
+            },
+        };
+
+
+        public User GetByEmail(string email)
+        {
+            return Users.Where(x => x.Email.ToLower() == email.ToLower())
+                .FirstOrDefault();
+        }
+    }
+}
